66537652a291e5c53421999bc88835e66d869274
ArlenZhang <longyin128128@163.com>
2018-03-14 16:19:36 +0800

    de

--- Demo16/test.py
+++
@@ -1,4 +0,0 @@
-import utils
-mnist_folder = '../../data/mnist'
-train_data, test_data = utils.get_mnist_dataset(1, mnist_folder=mnist_folder)
-print(train_data)
--- test.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-arr = np.array([[-1, -1, -3], [-4, -5, -6]])
-print(np.reshape(arr, [-1, 2]))
-
